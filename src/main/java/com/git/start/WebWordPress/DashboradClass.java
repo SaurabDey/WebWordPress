f115c98c20400710ba370280b5f34e91f8b7d641
SaurabDey <saurab.jeet@gmail.com>
2019-03-23 10:38:22 +0530

    I am adding code for Wordpress

---
+++ src/main/java/com/git/start/WebWordPress/DashboradClass.java
@@ -0,0 +1,19 @@
+package com.git.start.WebWordPress;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DashboradClass {
+	WebDriver driver;
+	public DashboradClass(WebDriver driver2) {
+		driver=driver2;
+	}
+	public void dashboradmethod()
+	{
+		WebElement clickPost=  driver.findElement(By.xpath("//div[text()='Posts']"));
+	      clickPost.click();
+	      WebElement clickAddnew=  driver.findElement(By.xpath("//a[text()='Add New'][@href='post-new.php']"));
+	      clickAddnew.click();
+	}
+}
---
+++ src/test/java/com/git/start/WebWordPress/WordPressAutomation.java
@@ -0,0 +1,61 @@
+package com.git.start.WebWordPress;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterTest;
+
+public class WordPressAutomation {
+	WebDriver driver;
+
+	@Test
+	public void f() {
+
+		LoginClass log = new LoginClass(driver);
+		log.loginMethod();
+
+		DashboradClass dash = new DashboradClass(driver);
+		dash.dashboradmethod();
+
+	}
+
+	@Parameters("bro")
+	@BeforeTest
+	public void beforeTest(String comingValue) {
+		
+		String browser=comingValue;
+		if (browser.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "Resource/chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		
+		else if (browser.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", "Resource/geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		
+		else if (browser.equals("internetExplorer")) {
+			System.setProperty("webdriver.ie.driver", "Resource/IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		}
+		
+		driver.get("http://demosite.center/wordpress/wp-login.php?");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@AfterTest
+	public void afterTest() throws InterruptedException {
+		driver.quit();
+	}
+
+}
