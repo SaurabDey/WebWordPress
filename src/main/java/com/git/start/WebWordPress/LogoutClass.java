4aba5b6d8ff0ec90c11fba0c21d56149d6332bac
SaurabDey <saurab.jeet@gmail.com>
2019-03-31 10:09:51 +0530

    Added file for logout

---
+++ src/main/java/com/git/start/WebWordPress/LogoutClass.java
@@ -0,0 +1,26 @@
+package com.git.start.WebWordPress;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class LogoutClass {
+	WebDriver driver;
+	public LogoutClass(WebDriver driver2) {
+		driver= driver2;
+	}
+	public void logoutMethod()
+	{
+		WebElement admin =  driver.findElement(By.id("wp-admin-bar-my-account"));
+		
+		WebElement logout =  driver.findElement(By.id("wp-admin-bar-logout"));
+		
+		Actions act= new Actions(driver);
+		act.moveToElement(admin).pause(Duration.ofSeconds(3)).moveToElement(logout).click().build().perform();
+		
+	}
+	
+}
