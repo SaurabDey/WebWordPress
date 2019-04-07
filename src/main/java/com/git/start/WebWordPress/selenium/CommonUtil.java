f7b83bac45029e5eb6890131b18491c20e132450
SaurabDey <saurab.jeet@gmail.com>
2019-04-07 12:35:51 +0530

    Code for excel

--- src/main/java/com/git/start/WebWordPress/selenium/CommonUtil.java
+++ src/main/java/com/git/start/WebWordPress/selenium/CommonUtil.java
@@ -1,11 +1,14 @@
 package com.git.start.WebWordPress.selenium;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -35,5 +38,34 @@ public class CommonUtil {
 			e.printStackTrace();
 		}
 		return dest;
+	}
+	
+	public String[] readExcelFile() {
+		File f = new File("Resource/dataWordPress.xlsx");
+		XSSFWorkbook excel = null;
+		FileInputStream fis = null;
+		String[] myvalue = null;
+		try {
+			fis = new FileInputStream(f);
+
+			excel = new XSSFWorkbook(fis);
+			XSSFSheet sheet = excel.getSheet("wordpress");
+
+			myvalue=new String[2];
+			myvalue[0]=sheet.getRow(1).getCell(0).getStringCellValue();;
+			myvalue[1]=sheet.getRow(1).getCell(1).getStringCellValue();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				excel.close();
+				fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return myvalue;
 	}
 }
